00553827e048c1ca99b677a439d5a45cf700585f
Motiejus Jakštys <desired.mta@gmail.com>
2011-05-02 16:25:54 +0100

    Initial rails (w/o Prototype) import

---
+++ trool/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trool::Application.config.secret_token = '640173f0f7e92ceb27b4243a00153e264d1266f8e3d9b0786080bb8c870f32adcd1ad58283a8bd403f46f48ed11f0d3f357924be220ea13bff76322e6bfbac72'
