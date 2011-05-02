c8cc62b6fcf35a11f32e74d71a5df6ccf523fc19
Motiejus Jakštys <desired.mta@gmail.com>
2011-05-02 23:51:17 +0100

    Parsing of pot headers; more testing

--- trool/app/models/pot.rb
+++ trool/app/models/pot.rb
@@ -1,35 +1,48 @@
+require 'date'
+
 class Pot < ActiveRecord::Base
   has_many :pos
 end
 
 # Returns hash from which Pot can be populated
 class PotInputParser
+  attr_reader :all_dict;
+
   def initialize(content)
-    @content = content
+    @pot = content
+    @all_dict = {}
   end
 
   def parse_copyright
     # Fourth line of the content
-    raise "bad @content" unless @content
-    line = @content.split("\n")[3]
-    all, @first_author, @first_author_email, @first_author_year =\
+    raise "bad @pot" unless @pot
+    line = @pot.split("\n")[3]
+    all, first_author, first_author_email, first_author_year =\
         line.match(/# (.*) <(.*)>, (\d+)/).to_a
-    if not (@first_author and @first_author_email and @first_author_year):
+    if not (first_author and first_author_email and first_author_year):
       raise "Wrong Line: %s expected author, email, year." % line
     end
-    @first_author_year = Integer(@first_author_year)
+
+    @all_dict[:first_author] = first_author
+    @all_dict[:first_author_email] = first_author_email
+    @all_dict[:first_author_year] = Integer(first_author_year)
   end
 
   def parse_headers
+    fetchable = {
+        :project_id_version => "Project-Id-Version: (.*)\\\\n",
+        :report_msgid_bugs_to => "Report-Msgid-Bugs-To: (.*)\\\\n",
+        :pot_creation_date => "POT-Creation-Date: (.*)\\\\n",
+    }
+    fetchable.each { |k,re| 
+        val = @pot.match(re)[1]
+        raise "Wrong %s: %s" % [k.replace(/: .*/, ''), val] unless val
+        @all_dict[k] = val
+    }
+    @all_dict[:pot_creation_date] = DateTime.parse(
+        @all_dict[:pot_creation_date])
   end
 
   def parse_messages
-  end
-
-  def all_dict
-    { :first_author => @first_author,
-        :first_author_email => @first_author_email,
-        :first_author_year => @first_author_year
-    }
   end
 end
--- trool/test/unit/pot_test.rb
+++ trool/test/unit/pot_test.rb
@@ -2,24 +2,42 @@ require 'test_helper'
 
 require 'rubygems'
 require 'ruby-debug'
+require 'date'
 Debugger.start
 
 class PotTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def setup
     @content = File.open('git.pot', 'r').read
-  end
-
-  test "load content" do
-    assert @content
-  end
-
-  test "get author, email and year" do
     parser = PotInputParser.new @content
     parser.parse_copyright
-    ret = parser.all_dict
-    assert_equal "Couch Dwellers", ret[:first_author]
-    assert_equal "couch@toostis.com", ret[:first_author_email]
-    assert_equal 2011, ret[:first_author_year]
+    parser.parse_headers
+    @ret = parser.all_dict
+  end
+
+  test "get author" do
+    assert_equal "Couch Dwellers", @ret[:first_author]
+  end
+
+  test "get first author email" do
+    assert_equal "couch@toostis.com", @ret[:first_author_email]
+  end
+
+  test "get first author year" do
+    assert_equal 2011, @ret[:first_author_year]
+  end
+
+  test "get project id version" do
+    assert_equal "1.7.5", @ret[:project_id_version]
+  end
+
+  test "get report msgid bugs to" do
+    assert_equal "Git Mailing List <git@vger.kernel.org>",
+      @ret[:report_msgid_bugs_to]
+  end
+  
+  test "get pot creation date" do
+    assert_equal DateTime.parse("2011-04-25 09:37+0100"),
+      @ret[:pot_creation_date]
   end
 end
