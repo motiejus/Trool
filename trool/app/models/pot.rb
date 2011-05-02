73ef87c66950162c7126c20137fce98316c495fc
Motiejus Jakštys <desired.mta@gmail.com>
2011-05-02 23:03:33 +0100

    Tests for primitive .pot file parsing

--- trool/app/models/pot.rb
+++ trool/app/models/pot.rb
@@ -1,3 +1,31 @@
 class Pot < ActiveRecord::Base
-    has_many :pos
+  has_many :pos
+end
+
+# Returns hash from which Pot can be populated
+class PotInputParser
+  def initialize(content)
+    @content = content
+  end
+
+  def parse_copyright
+    # Fourth line of the content
+    raise "bad @content" unless @content
+    line = @content.split("\n")[3]
+    all, @author, @email, @year = line.match(/# (.*) <(.*)>, (\d+)/).to_a
+    if not (@author and @email and @year):
+      raise "Wrong Line: %s expected author, email, year." % line
+    end
+    @year = Integer(@year)
+  end
+
+  def parse_headers
+  end
+
+  def parse_messages
+  end
+
+  def all_dict
+    { :author => @author, :email => @email, :year => @year }
+  end
 end
--- trool/test/unit/pot_test.rb
+++ trool/test/unit/pot_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
+require 'rubygems'
+require 'ruby-debug'
+Debugger.start
+
 class PotTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @content = File.open('git.pot', 'r').read
+  end
+
+  test "load content" do
+    assert @content
+  end
+
+  test "get author, email and year" do
+    parser = PotInputParser.new @content
+    parser.parse_copyright
+    ret = parser.all_dict
+    assert_equal "Couch Dwellers", ret[:author]
+    assert_equal "couch@toostis.com", ret[:email]
+    assert_equal 2011, ret[:year]
   end
 end
