6d9b717dfd44b9d30c5d387c8e3e63ee058fe4ec
Rimvydas Naktinis <naktinis@gmail.com>
2011-05-03 03:30:19 +0100

    Some messages traversal

--- trool/public/javascripts/application.js
+++ trool/public/javascripts/application.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     $('#messages input').change(function() {
         $(this).parents('form:first').submit();
+
+        // Hide current
+        $(this).hide()
+        $(this).parents('.msgstr:first').find('span').
+                text($(this).val()).show();
+
+        // Show next
+        $(this).parents('tr:first').next().
+                find('.msgstr span').hide();
+        $(this).parents('tr:first').next().
+                find('.msgstr input').show().focus();
+    }).keypress(function(e) {
+        if (e.keyCode == 13 || e.keyCode == 9) {
+            $(this).change();
+        }
     });
 });
