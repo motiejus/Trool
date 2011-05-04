d1147df10dff97e1871f7322b2508f1bd238deec
Motiejus Jakštys <desired.mta@gmail.com>
2011-05-04 01:14:15 +0100

    Hack fuzzy checkbox

--- trool/app/controllers/messages_controller.rb
+++ trool/app/controllers/messages_controller.rb
@@ -1,6 +1,14 @@
+require 'ruby-debug'
 class MessagesController < ApplicationController
   def update
     @msg = Message.find params[:id]
+
+    # TODO: learn ruby
+    fuzzy_arr = (params.keys.grep /fuzzy-\d+/) or []
+    fuzzy_bool = params[fuzzy_arr.first] == "1"
+
+    params[:msg] ||= {}
+    params[:msg].merge!( { :fuzzy => fuzzy_bool } )
     @msg.update_attributes params[:msg]
     render :nothing => true
   end
