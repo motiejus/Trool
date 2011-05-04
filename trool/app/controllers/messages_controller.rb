bd2ff49350465d48c9d1b76aaa683cba3a783780
Rimvydas Naktinis <naktinis@gmail.com>
2011-05-04 18:33:26 +0100

    Editing a message no longer removes fuzziness

--- trool/app/controllers/messages_controller.rb
+++ trool/app/controllers/messages_controller.rb
@@ -3,13 +3,16 @@ class MessagesController < ApplicationController
   def update
     @msg = Message.find params[:id]
 
-    # TODO: learn ruby
-    fuzzy_arr = (params.keys.grep /fuzzy-\d+/) or []
-    fuzzy_bool = params[fuzzy_arr.first] == "1"
+    # If fuzzy-id passed in request - change it
+    fuzzy_arr = params.keys.grep /fuzzy-\d+/
+    if not fuzzy_arr.empty?
+      params[:msg] ||= {}
+      params[:msg][:fuzzy] = params[fuzzy_arr.first] == "1"
+    end
 
-    params[:msg] ||= {}
-    params[:msg].merge!( { :fuzzy => fuzzy_bool } )
+    # Handle all other message parameters
     @msg.update_attributes params[:msg]
+
     render :nothing => true
   end
 end
