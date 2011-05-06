5faacfe2100e029f016b86b8b9b302bbe4a3b748
Rimvydas Naktinis <naktinis@gmail.com>
2011-05-06 17:41:26 +0100

    All po tests now pass

--- trool/test/unit/po_test.rb
+++ trool/test/unit/po_test.rb
@@ -6,21 +6,21 @@ class PoTest < ActiveSupport::TestCase
   def setup
     content = File.open(Rails.root.to_s+'/test/git.pot', 'r').read
     parser = PotInputParser.new content
-    pot = Pot.new({ :filedata => content }.merge parser.parse)
+    pot = Pot.new({ :filedata => content }.merge parser.parse_meta)
     @po = Po.new
     @po.pot = pot
-    @po.populate_messages
+    @po.populate_from_pot
   end
 
   test "generate string" do
     assert_equal "#: wt-status.c:134", @po.messages.first.header_string
-    assert_equal "msgid: \"Unmerged paths:\"\nmsgstr: \"\"",
+    assert_equal "msgid \"Unmerged paths:\"\nmsgstr \"\"",
         @po.messages.first.body_string
     assert_equal "#: wt-status.c:142 wt-status.c:159\n" +
                  "#, fuzzy, c_format, range: 3..23",
         @po.messages[2].header_string
-    assert_equal "#: wt-status.c:134\nmsgid: \"Unmerged paths:\""+
-                 "\nmsgstr: \"\"",
+    assert_equal "#: wt-status.c:134\nmsgid \"Unmerged paths:\""+
+                 "\nmsgstr \"\"",
         @po.messages.first.to_string
   end
 end
